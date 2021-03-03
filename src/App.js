56f7279efe5c7f41aba30dac34f5f951a9e633f5
fleckc3 <fleckc3@mail.dcu.ie>
2021-03-03 23:20:52 +0000

    errorhandler
    - added axios interceptors to handle request and response errors

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Layout from '../src/hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
+
   render() {
     return (
       <div>
         <Layout>
-
-          <BurgerBuilder/>
+          <BurgerBuilder/> 
         </Layout>
       </div>
     );
